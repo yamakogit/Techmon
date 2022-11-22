5731bca540c0cd8f5a4638295cb4686561ba6bf6
Koki Yamada <koki.y2764@gmail.com>
2022-11-22 09:08:39 +0900

    Initial Commit

---
+++ Techmon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techmon
+//
+//  Created by 山田航輝 on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
